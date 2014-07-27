c960fc41169b8fdc9d212d94336d0ba735645c1b
jefferson.almeida.comp@gmail.com <jefferson.almeida.comp@gmail.com>
2014-07-26 21:03:23 -0400

    syso

--- src/doublef/com/travis/Travis.java
+++ src/doublef/com/travis/Travis.java
@@ -7,7 +7,8 @@ public class Travis {
 	}
 	
 	public static void main(String[] args) {
-
+		System.out.println("Travis First Project");
+		System.out.println("method return:" + new Travis().method());
 	}
 
 }
